d909ca62f2559047c8499c0a35e8eef44a5e0cba
elianamuthui-coder <elianamuthui@gmail.com>
2026-07-03 13:36:45 +0300

    Finalize localStorage wishlist feature

--- .github/submission/js/Script.js
+++ .github/submission/js/Script.js
@@ -62,9 +62,7 @@ addBtn.addEventListener("click", function () {
         li.remove();
         localStorage.setItem("wishlist",wishlistItems.innerHTML);
     });
-    localStorage.setItem("theme", "dark");
-
-    let savedTheme = localStorage.getItem("theme");
+    
 
     li.appendChild(deleteBtn);
     wishlistItems.appendChild(li);
